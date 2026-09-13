de3b10c9a5377044e172b25113f4ffeeac5c3447
Paloma Besumbes <palomabesumbes@gmail.com>
2026-09-13 12:53:02 +0200

    feat: enable cors for frontend

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
 
   app.setGlobalPrefix('api');
 
